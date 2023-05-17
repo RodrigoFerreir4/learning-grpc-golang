36d9df1689779a9baee48dcf3322dcc78d92e566
Rodrigo4Dev <rodrigo.4dev@gmail.com>
2023-05-17 00:12:49 -0400

    feat: implement CategoryService

---
+++ internal/service/category.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Rodrigo4Dev/learning-grpc-golang/internal/database"
+	"github.com/Rodrigo4Dev/learning-grpc-golang/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+	return &pb.CategoryResponse{Category: categoryResponse}, nil
+}
